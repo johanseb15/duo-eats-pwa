d96cd845dfe5c40031e5e5d1b939841702d178ee
Johan Sebastian Molinares Gutierrez <sebasmol2010@gmail.com>
2025-08-15 16:36:06 +0000

    Continuemos

--- next.config.ts
+++ next.config.ts
@@ -7,12 +7,48 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development'
 })
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://maps.googleapis.com https://apis.google.com;
+  child-src 'self' https://google.com https://www.google.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: https://placehold.co https://firebasestorage.googleapis.com;
+  font-src 'self' data:;
+  connect-src 'self' https://firestore.googleapis.com https://identitytoolkit.googleapis.com wss://firestore.googleapis.com https://maps.googleapis.com;
+  frame-src 'self' https://duo-eats.firebaseapp.com;
+`;
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+   async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+           {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
   },
   images: {
     remotePatterns: [
